1e9ba7e3ed02473a190cb09bb98f887892f1a917
SudarshanShelke <sudarshanshelke@gmail.com>
2024-07-19 14:25:30 +0530

    Add JavaScript for calculator operations
    - Implemented functionality for number and operator buttons.
    - Added logic for performing calculations and displaying results.
    - Included error handling for invalid inputs.

---
+++ script.js
@@ -0,0 +1,159 @@
+var equation = document.getElementById('equation');
+var display = document.getElementById('display');
+
+
+var AC = document.getElementById('AC');
+var DEL = document.getElementById('DEL');
+var dot = document.getElementById('dot');
+var equal = document.getElementById('equal');
+
+var div = document.getElementById('div');
+var multi = document.getElementById('multi');
+var sub = document.getElementById('sub');
+var add = document.getElementById('add');
+
+var num1 = document.getElementById('num1');
+var num2 = document.getElementById('num2');
+var num3 = document.getElementById('num3');
+var num4 = document.getElementById('num4');
+var num5 = document.getElementById('num5');
+var num6 = document.getElementById('num6');
+var num7 = document.getElementById('num7');
+var num8 = document.getElementById('num8');
+var num9 = document.getElementById('num9');
+var num00 = document.getElementById('num00');
+var num0 = document.getElementById('num0');
+
+// // click event listener for num:
+// num1.addEventListener('click', function(){
+//     display.value += '1' ;
+// });
+// num2.addEventListener('click', function(){
+//     display.value += '2' ;
+// });
+// num3.addEventListener('click', function(){
+//     display.value += '3' ;
+// });
+// num4.addEventListener('click', function(){
+//     display.value += '4' ;
+// });
+// num5.addEventListener('click', function(){
+//     display.value += '5' ;
+// });
+// num6.addEventListener('click', function(){
+//     display.value += '6' ;
+// });
+// num7.addEventListener('click', function(){
+//     display.value += '7' ;
+// });
+// num8.addEventListener('click', function(){
+//     display.value += '8' ;
+// });
+// num9.addEventListener('click', function(){
+//     display.value += '9' ;
+// });
+// num00.addEventListener('click', function(){
+//     display.value += '00' ;
+// });
+// num00.addEventListener('click', function(){
+//     display.value += '0' ;
+// });
+
+// // click event listener for operator: 
+// div.addEventListener('click', function(){
+//     display.value += '/' ;
+// });
+// multi.addEventListener('click', function(){
+//     display.value += '*' ;
+// });
+// sub.addEventListener('click', function(){
+//     display.value += '-' ;
+// });
+// add.addEventListener('click', function(){
+//     display.value += '+' ;
+// });
+
+// // click event listener for function:
+// AC.addEventListener('click', function(){
+//     display.value = '' ;
+//     equation.value = '' ;
+// });
+// DEL.addEventListener('click', function(){
+//     display.value = display.value.toString().slice(0, -1)
+// });
+// dot.addEventListener('click', function(){
+//     display.value += '.' ;
+// });
+// equal.addEventListener('click', function(){
+//     equation.value = display.value ; 
+//     display.value = eval(display.value);
+// });
+
+
+
+// // keypress event listener for num:
+// num1.addEventListener('keypress', function(){
+//     display.value += '1' ;
+// });
+// num2.addEventListener('keypress', function(){
+//     display.value += '2' ;
+// });
+// num3.addEventListener('keypress', function(){
+//     display.value += '3' ;
+// });
+// num4.addEventListener('keypress', function(){
+//     display.value += '4' ;
+// });
+// num5.addEventListener('keypress', function(){
+//     display.value += '5' ;
+// });
+// num6.addEventListener('keypress', function(){
+//     display.value += '6' ;
+// });
+// num7.addEventListener('keypress', function(){
+//     display.value += '7' ;
+// });
+// num8.addEventListener('keypress', function(){
+//     display.value += '8' ;
+// });
+// num9.addEventListener('keypress', function(){
+//     display.value += '9' ;
+// });
+// num00.addEventListener('keypress', function(){
+//     display.value += '00' ;
+// });
+// num00.addEventListener('keypress', function(){
+//     display.value += '0' ;
+// });
+
+// // keypress event listener for operator: 
+// div.addEventListener('keypress', function(){
+//     display.value += '/' ;
+// });
+// multi.addEventListener('keypress', function(){
+//     display.value += '*' ;
+// });
+// sub.addEventListener('keypress', function(){
+//     display.value += '-' ;
+// });
+// add.addEventListener('keypress', function(){
+//     display.value += '+' ;
+// });
+
+// // keypress event listener for function:
+// AC.addEventListener('keypress', function(){
+//     display.value = '' ;
+//     equation.value = '' ;
+// });
+// DEL.addEventListener('keypress', function(){
+//     display.value = display.value.toString().slice(0, -1)
+// });
+// dot.addEventListener('keypress', function(){
+//     display.value += '.' ;
+// });
+// equal.addEventListener('keypress', function(){
+//     equation.value = display.value ; 
+//     display.value = eval(display.value);
+// });
+
+
